90b5b7794713dce2b6f980e122428bc07817056f
Vitoriacmlly <vcamilly18@gmail.com>
2025-05-22 12:27:19 -0300

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
+import Footer from './assets/components/footer/Footer';
+import Navbar from './assets/components/navbar/Navbar';
 import Home from './pages/home/Home';
 
 function App() {
