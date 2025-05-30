9dbfbb5d91183fac1ac4a215ae74005e41100cde
Vitoriacmlly <vcamilly18@gmail.com>
2025-05-30 10:17:14 -0300

    feat: adicionado a front de perfil atualizadoos alertas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,19 +1,27 @@
 import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { useContext, type ReactNode } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse as redes sociais</p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="www.linkedin.com/in/vitoria-camilly" target="_blank">
+                    <a href="www.linkedin.com/in/vitoria-camilly" target="_blank">
                         <LinkedinLogoIcon size={48} weight='bold' />
                         </a>
                         <a href="https://www.instagram.com/aypipinha/?igsh=MXJoNDg1dDIydTR4MQ%3D%3D#" target="_blank">
@@ -22,9 +30,15 @@ function Footer() {
                         <a href="https://github.com/Vitoriacmlly?tab=repositories" target="_blank">
                         <GithubLogoIcon size={48} weight='bold' />
                         </a>
-                        </div>
                     </div>
                 </div>
+            </div>
+
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
